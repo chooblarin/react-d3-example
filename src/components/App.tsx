79b16b7ead1f3ed9fb1e8db8d5ff7f0caf05f09a
Sota Hatakeyama <choo.bla.rin@gmail.com>
2018-12-10 16:22:00 +0900

    Mock data file

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,27 +5,11 @@ import BarChart from "./BarChart";
 import DonutChart from "./DonutChart";
 import TimeSeriesChart from "./TimeSeriesChart";
 import withMeasureAndRender from "./withMeasureAndRender";
-
-const donutInputData = [
-  { gender: "male", ratio: 0.2 },
-  { gender: "female", ratio: 0.8 }
-];
-
-const barInputData = [
-  { letter: "h", frequency: 50 },
-  { letter: "u", frequency: 30 },
-  { letter: "l", frequency: 15 },
-  { letter: "k", frequency: 5 }
-];
-
-const timeSeriesInputData = [
-  { time: `2014-11-15`, km: 789 },
-  { time: `2014-11-16`, km: 850 },
-  { time: `2014-11-17`, km: 880 },
-  { time: `2014-11-18`, km: 1086 },
-  { time: `2014-11-19`, km: 1285 },
-  { time: `2014-11-20`, km: 1398 }
-];
+import {
+  timeSeriesInputData,
+  barInputData,
+  donutInputData
+} from "../mock-data";
 
 const MeasuredBarChart = withMeasureAndRender(BarChart);
 const MeasuredTimeSeriesChart = withMeasureAndRender(TimeSeriesChart);
---
+++ src/mock-data.ts
@@ -0,0 +1,20 @@
+export const donutInputData = [
+  { gender: "male", ratio: 0.2 },
+  { gender: "female", ratio: 0.8 }
+];
+
+export const barInputData = [
+  { letter: "h", frequency: 50 },
+  { letter: "u", frequency: 30 },
+  { letter: "l", frequency: 15 },
+  { letter: "k", frequency: 5 }
+];
+
+export const timeSeriesInputData = [
+  { time: `2014-11-15`, km: 789 },
+  { time: `2014-11-16`, km: 850 },
+  { time: `2014-11-17`, km: 880 },
+  { time: `2014-11-18`, km: 1086 },
+  { time: `2014-11-19`, km: 1285 },
+  { time: `2014-11-20`, km: 1398 }
+];
